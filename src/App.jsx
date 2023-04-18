5a27aa20ca901c9cb84f36cd0f28e790e29036c7
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-18 12:13:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Component  from "./Components/concepts/04-react-18"
+
+function App() {
+  return (
+    <div className='container'>
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
